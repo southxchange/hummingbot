21af91f195cbcc928e59be426a2e95cceac63e55
Miguel Storani <miguelignaciostorani@gmail.com>
2021-08-02 13:07:09 -0300

    (feat) fix tests

--- test/connector/exchange/southxchange/test_southxchange_in_fight_order.py
+++ test/connector/exchange/southxchange/test_southxchange_in_fight_order.py
@@ -36,6 +36,8 @@ class TestSouthXchangeInFlightOrder(TestCase):
         assert in_flight_order.exchange_order_id == api_response["Code"]
         assert in_flight_order.trade_type == TradeType.SELL
         assert in_flight_order.amount == api_response["Amount"]
+        assert in_flight_order.trading_pair == "BTC2-USD2"
+        assert in_flight_order.price == api_response["LimitPrice"]
 
     def test_southxchange_object_from_api_response_buy(self):
         api_response = {
@@ -54,3 +56,23 @@ class TestSouthXchangeInFlightOrder(TestCase):
         assert in_flight_order.exchange_order_id == api_response["Code"]
         assert in_flight_order.trade_type == TradeType.BUY
         assert in_flight_order.amount == api_response["Amount"]
+        assert in_flight_order.trading_pair == "BTC2-USD2"
+        assert in_flight_order.price == api_response["LimitPrice"]
+
+    def test_in_flight_order_status_executed(self):
+        api_response = {
+            "Code": "dummyOrderId",
+            "Type": "sell",
+            "Amount": 123,
+            "LimitPrice": 123,
+            "ListingCurrency": "BTC2",
+            "ReferenceCurrency": "USD2",
+            "Status": "Executed",
+            "DateAdded": "2021-07-29T15:26:42.120Z"
+        }
+
+        in_flight_order = SouthXchangeInFlightOrder.from_json(api_response)
+
+        assert in_flight_order.is_done
+        assert not in_flight_order.is_failure
+        assert not in_flight_order.is_cancelled
